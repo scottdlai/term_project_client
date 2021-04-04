96da5a55a0c9ca9732b25a3a62bf65e37efa76bb
Scott Lai <tuonglai3602@gmail.com>
2021-04-03 17:06:42 -0700

    Used HashRouter instead.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  HashRouter,
+  // BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Admin from './components/Admin';
 import Home from './components/Home';
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter basename='/'>
       <Switch>
         <Route path='/admin' exact component={Admin} />
         <Route path='/' exact component={Home} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 };
 
