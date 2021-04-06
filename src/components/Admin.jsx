58c53a190e52bf3b6796b4d183a80b98cdcca0fb
Scott Lai <tuonglai3602@gmail.com>
2021-04-05 17:21:41 -0700

    Fixed some bugs.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,40 +1,56 @@
 import React, { useEffect, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
 import './Admin.css';
+import Login from './Login';
 
 const Admin = () => {
+  const { token } = useAuth();
   const [apiCounts, setAPICounts] = useState([]);
 
   useEffect(() => {
     const fetchAdmin = async () => {
-      const res = await fetch(
-        'https://comp-4537-term-project-7zchu.ondigitalocean.app/api/v0/admin'
-      );
-      const { apiCounts } = await res.json();
+      try {
+        const res = await fetch(
+          'https://comp-4537-term-project-7zchu.ondigitalocean.app/api/v0/admin',
+          { method: 'GET', headers: { Authorization: `bearer ${token}` } }
+        );
 
-      console.log(apiCounts);
-      setAPICounts(apiCounts);
+        if (!res.ok) {
+          throw new Error('');
+        }
+        const { apiCounts } = await res.json();
+
+        console.log(apiCounts);
+        setAPICounts(apiCounts);
+      } catch (error) {
+        setAPICounts([]);
+      }
     };
 
     fetchAdmin();
   }, []);
 
-  return apiCounts.length > 0 ? (
-    <div id='container-admin'>
-      <h1>Dash board</h1>
-      {apiCounts.map(({ apiName, count }) => {
-        const [method, endpoint] = apiName.split(' ');
+  return token ? (
+    apiCounts.length > 0 ? (
+      <div id='container-admin'>
+        <h1>Dash board</h1>
+        {apiCounts.map(({ apiName, count }) => {
+          const [method, endpoint] = apiName.split(' ');
 
-        return (
-          <div key={apiName} className='container'>
-            <span className='method'>{method}</span>
-            <span className='endpoint'>{endpoint}</span>
-            <span className='count'>{count}</span>
-          </div>
-        );
-      })}
-    </div>
+          return (
+            <div key={apiName} className='container'>
+              <span className='method'>{method}</span>
+              <span className='endpoint'>{endpoint}</span>
+              <span className='count'>{count}</span>
+            </div>
+          );
+        })}
+      </div>
+    ) : (
+      <h1>Loadding...</h1>
+    )
   ) : (
-    <h1>Loadding...</h1>
+    <Login />
   );
 };
 
